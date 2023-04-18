2f8bbe6c4bb00b106d59bc0b090703aecd90de1f
Louis Maes <louis.maes2@student.kuleuven.be>
2023-04-18 10:40:15 +0200

    2D grid conversion capacities

--- Clustering/Demand/capacities.py
+++ Clustering/Demand/capacities.py
@@ -2,16 +2,41 @@ import pandas as pd
 
 xls_capacity = pd.DataFrame()
 
-xls_capacity = pd.ExcelFile(f"C:/Users/Louis/Documents/Master/Thesis/GEP/GEP/xls_capacity.xlsx", engine = 'openpyxl')
+xls_capacity = pd.ExcelFile(f"C:/Users/Louis/iCloudDrive/Documents/Master/Thesis/DATA/xls_capacity.xlsx", engine = 'openpyxl')
 
 df_line = pd.read_excel(xls_capacity, 'Line')
 filtered_2030 = df_line[df_line['Year'] == 2030]
 filtered_CY = filtered_2030[filtered_2030['Climate Year'] == 2009]
 filtered_DE = filtered_CY[filtered_CY['Scenario'] == 'Distributed Energy']
-filtered_export = filtered_DE[filtered_DE['Parameter'] == 'Export Capacity (MW)']
-filtered_import = filtered_DE[filtered_DE['Parameter'] == 'Import Capacity (MW)']
-selected_export = filtered_export.loc[:, ['Node/Line', 'Value']]
-selected_import = filtered_import.loc[:, ['Node/Line', 'Value']]
+filtered_DE = filtered_DE.copy()
+# Rename
+filtered_DE['Node/Line'] = filtered_DE['Node/Line'].str.replace('0', '')
+filtered_rename = filtered_DE.rename(columns={'Value': 'Capacity (MW)'})
 
-selected_export.to_csv(f"C:/Users/Louis/Documents/Master/Thesis/GEP/GEP/export_capacity.csv")
-selected_import.to_csv(f"C:/Users/Louis/Documents/Master/Thesis/GEP/GEP/import_capacity.csv")
+
+
+filtered_export = filtered_rename[filtered_rename['Parameter'] == 'Export Capacity (MW)']
+filtered_import = filtered_rename[filtered_rename['Parameter'] == 'Import Capacity (MW)']
+selected_export = filtered_export.loc[:, ['Node/Line', 'Capacity (MW)']]
+selected_import = filtered_import.loc[:, ['Node/Line', 'Capacity (MW)']]
+
+## Group the DataFrame by index and aggregate the values
+
+# Split the "Node/Line" column into two separate columns
+selected_export[['Node1', 'Node2']] = selected_export['Node/Line'].str.split('-', expand=True)
+selected_export['Node1'] = selected_export['Node1'].str.slice(0, 2)
+selected_export['Node2'] = selected_export['Node2'].str.slice(0, 2)
+selected_export = selected_export.groupby(['Node1', 'Node2'])['Capacity (MW)'].sum().reset_index()
+# Pivot the data to get a 2D grid with countries as row and column labels
+selected_export = selected_export.pivot(index='Node1', columns='Node2', values='Capacity (MW)')
+
+# Split the "Node/Line" column into two separate columns
+selected_import[['Node1', 'Node2']] = selected_import['Node/Line'].str.split('-', expand=True)
+selected_import['Node1'] = selected_import['Node1'].str.slice(0, 2)
+selected_import['Node2'] = selected_import['Node2'].str.slice(0, 2)
+selected_import = selected_import.groupby(['Node1', 'Node2'])['Capacity (MW)'].sum().reset_index()
+# Pivot the data to get a 2D grid with countries as row and column labels
+selected_import = selected_import.pivot(index='Node1', columns='Node2', values='Capacity (MW)')
+
+selected_export.to_csv(f"C:/Users/Louis/Documents/Master/Thesis/GEP/Clustering/Demand/export_capacity.csv")
+selected_import.to_csv(f"C:/Users/Louis/Documents/Master/Thesis/GEP/Clustering/Demand/import_capacity.csv")
--- Clustering/Demand/demand.py
+++ Clustering/Demand/demand.py
@@ -6,7 +6,7 @@ CY = 2013
 df_demand = pd.DataFrame()
 
 # xls_dem = pd.ExcelFile( f"../Input Data/Demand_time_series/Demand_TimeSeries_{year}_{scenario}.xlsx",engine = 'openpyxl')
-xls_dem = pd.ExcelFile(f"C:/Users/Louis/Documents/Master/Thesis/GEP/Clustering/Demand kristof/Demand_TimeSeries_{year}_{scenario}.xlsx", engine = 'openpyxl')
+xls_dem = pd.ExcelFile(f"C:/Users/Louis/iCloudDrive/Documents/Master/Thesis/DATA/Demand_TimeSeries_{year}_{scenario}.xlsx", engine = 'openpyxl')
 
 counter = 0
 for sheet_name in xls_dem.sheet_names:
@@ -18,5 +18,53 @@ for sheet_name in xls_dem.sheet_names:
     df_demand[sheet_name] = df_sheet.iloc[row_number+1:row_number+8761,col_index_for_CY]
     counter += 1
 
+# Remove 0 from column names
+df_demand.columns = [col.replace('0', '') for col in df_demand.columns]
+
+# Rename the column
+df_demand.rename(columns={'DKE1': 'DK', 'SE1': 'SE', 'ITCN':'IT','LUB1':'LU','NOM1':'NO','UA1':'UA'}, inplace=True)
+
+## Sum columns
+# Denmark
+df_demand['DK'] = df_demand['DK'] + df_demand['DKW1']
+df_demand.drop('DKW1', axis=1, inplace=True)
+
+# France
+df_demand['FR'] = df_demand['FR'] + df_demand['FR15']
+df_demand.drop('FR15', axis=1, inplace=True)
+
+# Greece
+df_demand['GR'] = df_demand['GR'] + df_demand['GR3']
+df_demand.drop('GR3', axis=1, inplace=True)
+
+# Italy
+df_demand['IT'] = df_demand['IT'] + df_demand['ITCS'] + df_demand['ITN1'] + df_demand['ITS1'] + df_demand['ITSA'] + df_demand['ITSI']
+df_demand.drop('ITCS', axis=1, inplace=True)
+df_demand.drop('ITN1', axis=1, inplace=True)
+df_demand.drop('ITS1', axis=1, inplace=True)
+df_demand.drop('ITSA', axis=1, inplace=True)
+df_demand.drop('ITSI', axis=1, inplace=True)
+
+# Luxembourg
+df_demand['LU'] = df_demand['LU'] + df_demand['LUF1'] + df_demand['LUG1']
+df_demand.drop('LUF1', axis=1, inplace=True)
+df_demand.drop('LUG1', axis=1, inplace=True)
+
+# Sweden
+df_demand['SE'] = df_demand['SE'] + df_demand['SE2'] + df_demand['SE3'] + df_demand['SE4']
+df_demand.drop('SE2', axis=1, inplace=True)
+df_demand.drop('SE3', axis=1, inplace=True)
+df_demand.drop('SE4', axis=1, inplace=True)
+
+# Norway
+df_demand['NO'] = df_demand['NO'] + df_demand['NON1'] + df_demand['NOS']
+df_demand.drop('NON1', axis=1, inplace=True)
+df_demand.drop('NOS', axis=1, inplace=True)
+
+# UK
+df_demand['UK'] = df_demand['UK'] + df_demand['UKNI']
+df_demand.drop('UKNI', axis=1, inplace=True)
+
+
 # df_demand.to_csv(f"../Input Data/time_series_output/Demand_{year}_{scenario}_{CY}.csv")
-df_demand.to_csv(f"C:/Users/Louis/Documents/Master/Thesis/GEP/Clustering/Demand kristof/Demand_TimeSeries_output_{year}_{scenario}_{str(CY)}.csv")
+df_demand.to_csv(f"C:/Users/Louis/Documents/Master/Thesis/GEP/Clustering/Demand/Demand_TimeSeries_output_{year}_{scenario}_{str(CY)}.csv")
