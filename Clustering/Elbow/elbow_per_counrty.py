1f0eb65c458db8a8702979275f359cdfbd76862a
Louis Maes <louis.maes2@student.kuleuven.be>
2023-04-26 18:20:53 +0200

    data + elbow

---
+++ Clustering/Elbow/elbow_per_counrty.py
@@ -0,0 +1,124 @@
+from sklearn.cluster import KMeans
+
+#from ..BaseClass import BaseSpOptHeuristicSolver
+import netCDF4 as nc
+from spopt.region import WardSpatial
+import matplotlib.pyplot as plt
+
+import libpysal
+import matplotlib
+import numpy as np
+import spopt
+import warnings
+import csv
+import geopandas as gpd
+import time
+import pandas as pd
+import pytest
+from shapely.geometry import Point, Polygon
+import json
+
+### This algorithm deducts the optimal number of clusters k using the heuristic elbow mehtod
+
+# Skip warnings
+warnings.filterwarnings("ignore")
+
+# fn_era = 'C:/Users/defor/OneDrive/Bureaublad/unif/Master/Thesis/GEP/Data/data_clustering/europe-2013-era5.nc'
+fn_era = "C:/Users/Louis/iCloudDrive/Documents/Master/Thesis/DATA/europe-2013-era5.nc"
+
+ds = dict()
+ds["w"] = nc.Dataset(fn_era)
+RANDOM_SEED = 123456
+
+### Select smaller range of both datapoints
+lon = ds["w"]["lon"][:]
+lat = ds["w"]["lat"][:]
+wm = ds["w"]["wnd100m"][:,:,:]
+id = ds["w"]["influx_direct"][:,:,:]
+
+## Only select first res values of each for threshold=number of points you should take together
+res = 141
+# k_range = range(7, 50)
+
+lenlon = len(lon)
+lenlat = len(lat)
+lon = lon[:-(lenlon-res)]
+lat = lat[:-(lenlat-res)]
+
+## Transform the lists
+lon = np.tile(lon, res)
+lat = np.repeat(lat, res)
+
+## Preperation for GeoDataFrame
+geo = gpd.GeoSeries.from_xy(lon, lat)
+w = libpysal.weights.lat2W(res, res)
+
+## Read EEZ file
+EEZ = gpd.read_file("C:/Users/Louis/Documents/Master/Thesis/GEP/GEP/EEZ/Europe/EEZ_Europe_extended_3.geojson")
+
+### Wind
+wm = np.average(wm,axis=0)
+wm = wm[:-(lenlat-res),:-(lenlon-res)]
+wm = list(np.concatenate(wm).flat)
+
+## transform to GeoDataFrame
+frame = gpd.GeoDataFrame(wm, geometry=geo)
+geometry = frame.geometry
+
+# create countrie dictionary
+dict_country = dict()
+
+## make a dictionary with key = country names and values = list with wind values
+for country_name in EEZ["UNION"]:
+    dict_country[country_name] = list()
+    print(country_name)
+    for number in range(len(wm)):
+        for polygon in EEZ[EEZ["UNION"] == country_name]['geometry']:
+            if polygon.contains(geometry.iloc[number]):
+                dict_country[country_name].append(wm[number])
+print(dict_country)
+
+assigned = 0
+for value in dict_country:
+    assigned += len(dict_country[value])
+print('Assigned points ', assigned)
+
+## calculate within-cluster sum of squares
+wss = dict()
+for key, value in dict_country.items():
+    wss[key] = sum([(x - np.mean(value)) ** 2 for x in value])
+print('within-cluster sum of squares', wss)
+
+
+## Sun
+id = np.average(id,axis=0)
+id = id[:-(lenlat-res),:-(lenlon-res)]
+id = list(np.concatenate(id).flat)
+
+## transform to GeoDataFrame
+frame = gpd.GeoDataFrame(id, geometry=geo)
+geometry = frame.geometry
+
+# create countrie dictionary
+dict_country = dict()
+
+## make a dictionary with key = country names and values = list with wind values
+for country_name in EEZ["UNION"]:
+    dict_country[country_name] = list()
+    print(country_name)
+    for number in range(len(id)):
+        for polygon in EEZ[EEZ["UNION"] == country_name]['geometry']:
+            if polygon.contains(geometry.iloc[number]):
+                dict_country[country_name].append(id[number])
+print(dict_country)
+
+assigned = 0
+for value in dict_country:
+    assigned += len(dict_country[value])
+print('Assigned points ', assigned)
+
+## calculate within-cluster sum of squares
+wss = dict()
+for key, value in dict_country.items():
+    wss[key] = sum([(x - np.mean(value)) ** 2 for x in value])
+print('within-cluster sum of squares', wss)
--- Clustering/maxp.py
+++ Clustering/maxp.py
@@ -43,15 +43,15 @@ lat = np.repeat(lat, res)
 
 ## Preperation for GeoDataFrame
 geo = gpd.GeoSeries.from_xy(lon, lat)
-# w = libpysal.weights.lat2W(res, res)
+w = libpysal.weights.lat2W(res, res)
 
-# define number of neighbors for KNN
-k = 3
+# # define number of neighbors for KNN
+# k = 3
 
-# compute KNN weights
-coords = list(zip(lon, lat))
-knn_graph = kneighbors_graph(coords, k, mode='distance', include_self=False)
-w = libpysal.weights.KNN(coords, k)
+# # compute KNN weights
+# coords = list(zip(lon, lat))
+# knn_graph = kneighbors_graph(coords, k, mode='distance', include_self=False)
+# w = libpysal.weights.KNN(coords, k)
 
 ### Wind
 start_wind = time.time()
